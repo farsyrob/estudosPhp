89c975c2d61eaa077486a921dccc7ae1f5024c2a
Rob Farsy <90431486+farsyrob@users.noreply.github.com>
2021-10-05 16:55:59 -0300

    Add files via upload

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = [1,2,3,4,5];
+
+echo $a[3]."<br>";
+print_r($a);
+echo "<br>";
+
+$b = ["Greg",1005,true];
+
+echo $b[0]."<br>";
+print_r($b);
+echo"<br>";
+print_r($b[2]);
---
+++ arrayAss.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = ['nome' => 'greg', 'idade' => 29, 'altura' => 1.94];
+
+echo $a['nome']."<br>";
+echo $a['idade']."<br>";
+echo $a['altura']."<br>";
+print_r($a);
+
